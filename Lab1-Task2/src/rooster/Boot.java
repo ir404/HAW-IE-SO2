21764eb9cf51889db47a28f89d3b6f0f488dd480
imran <imranrizwan1115@gmail.com>
2025-05-16 16:25:54 +0200

    Fixed the Task 5 by overloading methods in the Boot and Wing classes. Also moved the randomisation to the drawAt method of Body.

--- Lab1-Task2/src/rooster/Boot.java
+++ Lab1-Task2/src/rooster/Boot.java
@@ -11,11 +11,22 @@ public class Boot {
         this.colour = colour;
     }
 
-    public void drawAt(int left, int bottom) {        // 700, 650
+    public void drawAt(int left, int bottom) {
         int[] xCoords = {left, left, left - 50, left + 30, left + 30};
         int[] yCoords = {bottom, bottom + 25, bottom + 50, bottom + 50, bottom};
 
         Drawing.pen().setColor(this.colour);
         Drawing.pen().fillPolygon(xCoords, yCoords, 5);
+    }
+
+    /**
+     * Draws a boot with spurs (like a cowboy boot)
+     * */
+    public void drawAt(int left, int bottom, Color spurColour) {
+        drawAt(left, bottom);
+
+        Drawing.pen().setColor(spurColour);
+        Drawing.pen().fillRect(left + 30, bottom + 25, 15, 10);
+        Drawing.pen().fillOval(left + 40, bottom + 15, 25, 25);
     }
 }
--- Lab1-Task2/src/rooster/Wing.java
+++ Lab1-Task2/src/rooster/Wing.java
@@ -18,7 +18,19 @@ public class Wing {
         Drawing.pen().fillPolygon(xCoords, yCoords, 4);
     }
 
-    public Color getColour() {
-        return colour;
+    /**
+     * Draws a wing with rockets
+     */
+    public void drawAt(int left, int bottom, Color rocketColour) {
+        drawAt(left, bottom);
+
+        int[] xCoords1 = {left + 10, left + 15, left + 100, left + 100, left + 15, left + 10};
+        int[] yCoords1 = {bottom + 15, bottom + 10, bottom + 10, bottom + 20, bottom + 20, bottom + 15};
+        int[] xCoords2 = {left + 10, left + 15, left + 75, left + 75, left + 15, left + 10};
+        int[] yCoords2 = {bottom + 30, bottom + 25, bottom + 25, bottom + 35, bottom + 35, bottom + 30};
+
+        Drawing.pen().setColor(rocketColour);
+        Drawing.pen().fillPolygon(xCoords1, yCoords1, 6);
+        Drawing.pen().fillPolygon(xCoords2, yCoords2, 6);
     }
 }
