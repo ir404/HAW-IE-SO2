db71080d394e91d2e493e597f022322e3c77f112
imran <imranrizwan1115@gmail.com>
2025-05-16 16:36:51 +0200

    Revert "Fixed the Task 5 by overloading methods in the Boot and Wing classes. Also moved the randomisation to the drawAt method of Body."
    This reverts commit 21764eb9cf51889db47a28f89d3b6f0f488dd480.

--- Lab1-Task2/src/rooster/Body.java
+++ Lab1-Task2/src/rooster/Body.java
@@ -12,37 +12,38 @@ public class Body {
     private Tail tail;                  // composite
 
     public Body(Color colour, Color wingColour, Color bootColour, Color tailColour) {
+        int number = RandomNumber.between(1, 30);
         this.colour = colour;
         boot = new Boot(bootColour);
         tail = new Tail(tailColour);
-        wing = new Wing(wingColour);
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            wing = new RocketWing(wingColour, Color.RED);
+            boot = new CowboyBoot(bootColour);
+        }
+        else if (number % 3 == 0) {
+            wing = new RocketWing(wingColour, Color.RED);
+            boot = new Boot(bootColour);
+        }
+        else if (number % 5 == 0) {
+            wing = new Wing(wingColour);
+            boot = new CowboyBoot(bootColour);
+        }
+        else {
+            wing = new Wing(wingColour);
+            boot = new Boot(bootColour);
+        }
     }
 
     public void drawAt(int left, int bottom) {
         int[] xCoords = {left, left - 25, left - 25, left + 50, left + 80, left + 200, left};
         int[] yCoords = {bottom, bottom + 25, bottom + 50, bottom + 150, bottom + 150, bottom, bottom};
-        int number = RandomNumber.between(1, 100);
 
         Drawing.pen().setColor(this.colour);
         Drawing.pen().fillPolygon(xCoords, yCoords, 7);
 
+        wing.drawAt(left + 25, bottom);
+        boot.drawAt(left + 50, bottom + 150);
         tail.drawAt(left + 200, bottom);
-
-        if (number % 3 == 0 && number % 5 == 0) {
-            wing.drawAt(left + 25, bottom, Color.RED);
-            boot.drawAt(left + 50, bottom + 150, Color.ORANGE);
-        }
-        else if (number % 3 == 0) {
-            wing.drawAt(left + 25, bottom, Color.RED);
-            boot.drawAt(left + 50, bottom + 150);
-        }
-        else if (number % 5 == 0) {
-            wing.drawAt(left + 25, bottom);
-            boot.drawAt(left + 50, bottom + 150, Color.ORANGE);
-        }
-        else {
-            wing.drawAt(left + 25, bottom);
-            boot.drawAt(left + 50, bottom + 150);
-        }
     }
 }
